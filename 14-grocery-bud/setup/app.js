2f0fab63d93978af77958a0b4fc38be5a9c1d721
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2022-06-29 17:16:21 -0400

    Finished Grocery bud project

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -15,6 +15,11 @@ let editID = '';
 // ****** EVENT LISTENERS **********
 // submit form
 form.addEventListener('submit',addItem)
+// clear items
+clearBtn.addEventListener('click', clearItems);
+// load items
+window.addEventListener('DOMContentLoaded', setupItems);
+
 
 // ****** FUNCTIONS **********
 function addItem(e){
@@ -23,9 +28,22 @@ function addItem(e){
 
  const id = new Date().getTime().toString()
  if(value && !editFlag){
-  console.log('add item to the list')
+  createListItem(id,value);
+   // display alert
+   displayAlert('item added to the list', 'success');
+   // show container
+   container.classList.add('show-container');
+   // add to local storage
+   addToLocalStorage(id, value);
+   // set back to default
+   setBackToDefault();
+   console.log('add item to the list')
  } else if(value && editFlag) {
-  console.log('editing')
+  editElement.innerHTML = value;
+  displayAlert('value changed', 'success');
+  // edit local storage
+  editLocalStorage(editID, value);
+  setBackToDefault();
  } else {
   displayAlert('please enter value', "danger")
  }
@@ -41,7 +59,125 @@ function displayAlert(text, action) {
   alert.classList.remove(`alert-${action}`);
  }, 1000);
 }
-
+// clear items
+function clearItems(){
+ const items = document.querySelectorAll('.grocery-item');
+ if(items.length > 0){
+  items.forEach(function(item){
+   list.removeChild(item);
+  });
+ }
+ container.classList.remove('show-container');
+ displayAlert('empty list', 'danger');
+ setBackToDefault();
+ localStorage.removeItem('list');
+}
+// delete function
+ function deleteItem(e){
+  const element = e.currentTarget.parentElement.parentElement;
+  const id = element.dataset.id;
+  list.removeChild(element);
+  if(list.children.length === 0){
+   container.classList.remove('show-container')
+  }
+  displayAlert('item removed', 'danger');
+  setBackToDefault();
+  // remove from local storage
+  removeFromLocalStorage(id);
+ }
+// edit function
+ function editItem(e){
+  const element = e.currentTarget.parentElement.parentElement;
+  // set edit item
+  editElement = e.currentTarget.parentElement.previousElementSibling;
+  // set form value
+  grocery.value = editElement.innerHTML
+  editFlag = true;
+  editID = element.dataset.id;
+  submitBtn.textContent = 'edit';
+ }
+// set back to default
+function setBackToDefault(){
+ grocery.value = '';
+ editFlag = false;
+ editID = '';
+ submitBtn.textContent = 'submit';
+}
 // ****** LOCAL STORAGE **********
-
+function addToLocalStorage(id, value){
+ const grocery = {id:id, value:value};
+ // console.log(grocery)
+ let items = getLocalStorage();
+ console.log(items)
+ items.push(grocery);
+ localStorage.setItem('list',JSON.stringify(items));
+}
+function removeFromLocalStorage(id){
+ let items = getLocalStorage();
+ items = items.filter(function(item){
+  if(item.id !== id){
+   return item
+  }
+ });
+ localStorage.setItem('list',JSON.stringify(items));
+}
+function editLocalStorage(id, value){
+ let items = getLocalStorage();
+ items = items.map(function(item){
+  if(item.id !== id){
+   item.value = value;
+  }
+  return item;
+ })
+}
+function getLocalStorage(){
+ return localStorage.getItem('list')?JSON.parse(localStorage.getItem('list')): [];
+}
+// localStorage API
+// setItem
+// getItem
+// removeItem
+// save as strings
+// localStorage.setItem('orange', JSON.stringify(['item','item2'])); - set item
+// const oranges = JSON.parse(localStorage.getItem('orange')); - get item
+// console.log(oranges);
+// localStorage.removeItem('orange'); - remove item
 // ****** SETUP ITEMS **********
+function setupItems(){
+ let items = getLocalStorage();
+ if(items.length > 0) {
+  items.forEach(function(item){
+   createListItem(item.id, item.value);
+  })
+  container.classList.add('show-container')
+ }
+}
+
+function createListItem(id, value){
+ const element = document.createElement('article');
+  // add class
+  element.classList.add('grocery-item');
+  // add id
+  const attr = document.createAttribute('data-id');
+  attr.value = id;
+  element.setAttributeNode(attr);
+  element.innerHTML = `
+   <p class="title">${value}</p>
+   <div class="btn-container">
+     <button type="button" class="edit-btn">
+       <i class="fas fa-edit"></i>
+     </button>
+     <button type="button" class="delete-btn">
+       <i class="fas fa-trash"></i>
+     </button>
+   </div>`;
+  const deleteBtn = element.querySelector('.delete-btn');
+  const editBtn = element.querySelector('.edit-btn');
+  deleteBtn.addEventListener('click', deleteItem);
+  editBtn.addEventListener('click', editItem);
+
+   // append child
+   list.appendChild(element);
+};
+
+// FIX THE EDITING LOCAL STORAGE
